21ca5e1bffd1dc3353fb995f02e8f177e17c31c4
usrrname <usrrname@users.noreply.github.com>
2017-08-23 11:19:39 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,234 @@
+/*
+	I modded the Metronic by TEMPLATE STOCK
+	templatestock.co @templatestock
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+(function($) {
+	"use strict";
+	
+		/*====================================
+		 Bootstrap Fix For WinPhone 8 And IE10
+		======================================*/
+		
+		if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+			var msViewportStyle = document.createElement("style");
+			msViewportStyle.appendChild(
+				document.createTextNode(
+					"@-ms-viewport{width:auto!important}"
+				)
+			);
+			document.getElementsByTagName("head")[0].
+				appendChild(msViewportStyle);
+		}	
+		
+		
+		/*====================================
+					Android stock browser
+		======================================*/
+		
+		$(function () {
+		  var nua = navigator.userAgent
+		  var isAndroid = (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1)
+		  if (isAndroid) {
+			$('select.form-control').removeClass('form-control').css('width', '100%')
+		  }
+		})	
+
+
+
+		/*====================================
+					Preloader
+		======================================*/
+
+		$(window).load(function() {
+		
+			var preloaderDelay = 350,
+				preloaderFadeOutTime = 800;
+	
+			function hidePreloader() {
+				var loadingAnimation = $('#loading-animation'),
+					preloader = $('#preloader');
+	
+				loadingAnimation.fadeOut();
+				preloader.delay(preloaderDelay).fadeOut(preloaderFadeOutTime);
+			}
+	
+			hidePreloader();
+	
+		});
+		
+		
+		
+		/*====================================
+					Background
+		======================================*/
+		
+		//Image Background 
+		//$(".image-background").backstretch("images/image-bg.jpg");
+		
+		//Parallax Background 
+		if($('body').hasClass('parallax-background')) {
+					
+		$.parallaxify({
+			positionProperty: 'transform',
+			responsive: true,
+			motionType: 'natural',
+			mouseMotionType: 'performance',
+			motionAngleX: 70,
+			motionAngleY: 70,
+			alphaFilter: 0.5,
+			adjustBasePosition: true,
+			alphaPosition: 0.025,
+		});
+	    } 
+		
+		//Particle Background 
+		$(".particle-background").backstretch("../../assets/images/bg/particle-bg.jpg");
+		
+		$('.particles').particleground({
+		dotColor: '#5cbdaa',
+		lineColor: '#5cbdaa',
+		parallax: false,
+		});
+		//Vegas background carousel 
+		$('.image-background').backstretch("images/image-bg.png").vegas({
+			delay: 8000,
+			timer: false,
+			shuffle: true,
+			firstTransition: 'fade',
+			firstTransitionDuration: 4000,
+			transition: 'fade2',
+			transitionDuration: 3500,
+			slides: [
+				{ src: './images/slide1.jpg' },
+				{ src: './images/slide2.jpg' },
+				{ src: './images/slide3.jpg' },
+				{ src: './images/slide4.jpg' },
+				{ src: './images/slide5.jpg' },
+				{ src: './images/slide6.jpg' }
+			]
+		});
+		
+		/*====================================
+					Clock Countdown
+		======================================*/
+
+		$('#clock-countdown').countdown('2017/10/23 12:00:00').on('update.countdown', function(event) {
+			var $this = $(this).html(event.strftime(''
+				+ '<div class="counter-container"><div class="counter-box first"><div class="number">%-D</div><span>Day%!d</span></div>'
+				+ '<div class="counter-box"><div class="number">%H</div><span>Hours</span></div>'
+				+ '<div class="counter-box"><div class="number">%M</div><span>Minutes</span></div>'
+				+ '<div class="counter-box last"><div class="number">%S</div><span>Seconds</span></div></div>'
+			));
+		});
+
+		/*====================================
+					Nice Scroll
+		======================================*/
+			
+		$("html").niceScroll({
+			cursorcolor: '#ccc',
+			cursoropacitymin: '0',
+			cursoropacitymax: '1',
+			cursorwidth: '3px',
+			zindex: 10000,
+			horizrailenabled: false,
+		});
+
+				
+			
+		/*====================================
+					Animated.css
+		======================================*/
+
+		$('.animated').appear(function() {
+			var element = $(this),
+				animation = element.data('animation'),
+				animationDelay = element.data('animation-delay');
+				if ( animationDelay ) {
+	
+					setTimeout(function(){
+						element.addClass( animation + " visible");
+					}, animationDelay);
+	
+				} else {
+					element.addClass( animation + " visible");
+				}
+		});
+
+
+			
+
+
+		/*====================================
+					Contact Form
+		======================================*/
+		
+		function initContactForm() {
+
+			var scrollElement = $('html,body'),
+				contactForm = $('.contact-form'),
+				form_msg_timeout;
+
+			contactForm.on( 'submit', function() {
+
+				var requiredFields = $(this).find('.required'),
+					formData = contactForm.serialize(),
+					formAction = $(this).attr('action'),
+					formSubmitMessage = $('.response-message');
+
+				requiredFields.each(function() {
+
+					if( $(this).val() == "" ) {
+
+						$(this).addClass('input-error');
+
+					} else {
+
+						$(this).removeClass('input-error');
+					}
+
+				});
+
+				function validateEmail(email) { 
+					var exp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+					return exp.test(email);
+				}
+
+				var emailField = $('.contact-form-email');
+
+				if( !validateEmail(emailField.val()) ) {
+
+					emailField.addClass("input-error");
+
+				}
+
+				if ($(".contact-form :input").hasClass("input-error")) {
+					return false;
+				} else {
+				
+					clearTimeout(form_msg_timeout);
+					
+					$.post(formAction, formData, function(data) {
+						formSubmitMessage.text(data);
+
+						requiredFields.val("");
+
+						form_msg_timeout = setTimeout(function() {
+							formSubmitMessage.slideUp();
+						}, 5000);
+					});
+
+				}
+
+				return false;
+
+			});
+
+		}
+		initContactForm();
+			
+
+
+})(jQuery);
